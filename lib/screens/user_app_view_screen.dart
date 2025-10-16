ecf041c9a2750580ea1987339641ecad32749355
kkspeed <685990+kkspeed@users.noreply.github.com>
2025-10-16 16:05:57 -0700

    save note avoids file name conflict

--- lib/screens/user_app_view_screen.dart
+++ lib/screens/user_app_view_screen.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 import '../l10n/app_localizations.dart';
 import '../providers/app_provider.dart';
 import '../models/user_app.dart';
@@ -1577,15 +1578,26 @@ class _UserAppViewScreenState extends State<UserAppViewScreen> {
       // Decode base64 data
       final bytes = base64Decode(base64String);
 
+      // Generate UUID to prevent file overwrites
+      const uuid = Uuid();
+      final uniqueId = uuid.v4();
+      
+      // Extract file extension
+      final fileExtension = fileName.contains('.') ? '.${fileName.split('.').last}' : '';
+      final baseFileName = fileName.contains('.') ? fileName.substring(0, fileName.lastIndexOf('.')) : fileName;
+      
+      // Create unique filename with UUID prefix
+      final uniqueFileName = '${baseFileName}_$uniqueId$fileExtension';
+
       // Get cache directory
       final cacheDir = await getTemporaryDirectory();
-      final filePath = '${cacheDir.path}/$fileName';
+      final filePath = '${cacheDir.path}/$uniqueFileName';
 
       // Write file to cache directory
       final file = File(filePath);
       await file.writeAsBytes(bytes);
 
-      LoggerService.debug('[Synapse.saveNotes] Saved base64 attachment: $fileName (${bytes.length} bytes) to $filePath');
+      LoggerService.debug('[Synapse.saveNotes] Saved base64 attachment: $fileName -> $uniqueFileName (${bytes.length} bytes) to $filePath');
       
       return filePath;
     } catch (e) {
