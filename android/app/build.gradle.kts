479555162351f37d721e4103cac2ea556f2e2a60
kkspeed <685990+kkspeed@users.noreply.github.com>
2026-02-05 16:30:37 -0800

    remove firebase analytics

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,8 +64,3 @@ flutter {
     source = "../.."
 }
 
-
-dependencies {
-  implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
-  implementation("com.google.firebase:firebase-analytics")
-}
