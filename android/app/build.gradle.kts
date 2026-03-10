202e3e88466b2566567bddb74bd6a977986c255d
kkspeed <685990+kkspeed@users.noreply.github.com>
2026-03-09 18:49:02 -0700

    update build script to not need key.properties.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,10 +40,12 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
